45304ee012c99919bc72a5f5ece128dd2212fa7e
Bruno Filadelfo <Bruno.Filadelfo@Outlook.Com>
2023-03-07 01:01:50 -0300

    continuação do projeto

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,17 @@
 import './style.css';
 
+import { Card } from '../../components/card';
+
 export function Home() {
   return (
     <div className='container'>
     <h1>Lista de Presença</h1>
     <input type="text" placeholder="Digite o nome..."/>
     <button type="button">Adicionar</button>
+
+    <Card name="Bruno Filadelfo" time="08:30:00" />
+    <Card name="Lucas Barbosa" time="09:00:30" />
+    <Card name="Aline Souza" time="09:15:40" />
     </div>
   )
 }
